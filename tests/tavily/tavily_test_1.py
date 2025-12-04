9628d88b564b4f9b27a8906ca6a31becb6396652
jomkeu <74103004+jomkeu3369@users.noreply.github.com>
2025-12-04 13:18:58 +0900

    Delete tests directory

--- tests/tavily/tavily_test_1.py
+++
@@ -1,7 +0,0 @@
-from tavily import TavilyClient
-
-client = TavilyClient("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")
-response = client.search(
-    query="디스코드py를 이용하여 음악 재생 봇 제작 비용"
-)
-print(response)
