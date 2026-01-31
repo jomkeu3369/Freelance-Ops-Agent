6da7e25fdb00369bbb20fc53249bd4623242d655
git-seungwon <tmddnjs3369@naver.com>
2026-01-31 18:06:56 +0900

    auth scheme 수정

--- src/api/auth/auth_router.py
+++ src/api/auth/auth_router.py
@@ -24,7 +24,7 @@ REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7))
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/stack/api/v1/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
