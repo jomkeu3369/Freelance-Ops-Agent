51af2c48c5a0c18710510454bc40215e588a941e
jomkeu <74103004+jomkeu3369@users.noreply.github.com>
2026-09-01 16:29:13 +0900

    fix: 운영 DB 확장 준비 절차 추가

--- infra/postgres/init/001-init.sh
+++ infra/postgres/init/001-init.sh
@@ -7,6 +7,7 @@ psql --set=ON_ERROR_STOP=1 \
   --set=app_password="$APP_DB_PASSWORD" \
   --set=agent_password="$AGENT_DB_PASSWORD" <<'SQL'
 CREATE EXTENSION IF NOT EXISTS vector;
+CREATE EXTENSION IF NOT EXISTS btree_gist;
 
 CREATE ROLE app_user LOGIN PASSWORD :'app_password';
 CREATE ROLE agent_user LOGIN PASSWORD :'agent_password';
@@ -17,4 +18,3 @@ CREATE SCHEMA agent_runtime AUTHORIZATION agent_user;
 GRANT CONNECT ON DATABASE freelance_ops TO app_user, agent_user;
 REVOKE CREATE ON SCHEMA public FROM PUBLIC;
 SQL
-
